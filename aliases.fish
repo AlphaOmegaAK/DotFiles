ef764b7324b4a773c7ef0b05ded47bc061ce4846
AlphaOmegaAK <alphaunionxt@gmail.com>
2021-04-01 11:32:40 -0400

    update

---
+++ aliases.fish
@@ -0,0 +1,34 @@
+alias v="nvim"
+alias cl="clear"
+
+alias so="source "
+
+
+alias g ="git "
+alias gs="git status -sb"
+alias gaa="git add -A"
+alias gcmsg="git commit -m "
+alias gb="git branch"
+alias gd="git diff"
+alias gpush="git push"
+alias gpull="git pull"
+alias grim="git rebase -i master"
+
+alias lsd="ls -d .*"
+alias la="ls -a"
+
+alias ..="cd .. && ls"
+alias ...="cd ../.. && ls"
+alias dots="nvim ~/Code/DotFiles/"
+alias cdcd="cd ~/Code/ && ls"
+alias cdpy="cd ~/Code/Python && ls"
+alias cdja="cd ~/Code/Java && ls"
+alias cdjs="cd ~/Code/Javascript && ls"
+alias cdts="cd ~/Code/TypeScript && ls"
+alias cdrs="cd ~/Code/Rust && ls"
+alias cdcpp="cd ~/Code/C++ && ls"
+alias cdbs="cd ~/Code/Scripts && ls"
+alias cddf="cd ~/Code/DotFiles && ls"
+alias cdcc="cd ~/Code/Code_Challenges && ls"
+alias cddl="cd ~/Downloads && ls"
+
---
+++ config.fish
@@ -0,0 +1,3 @@
+starship init fish | source
+
+source ~/Code/DotFiles/aliases.fish
