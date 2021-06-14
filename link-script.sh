e2620263c4df8468bd914d549f377afd672eaf93
alpha0megaak <arturxkaminski@gmail.com>
2021-06-14 11:36:31 -0400

    Update to base system config files

--- link-script.sh
+++ link-script.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
 ln -s $(pwd)/Alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml ;
+
 ln -s $(pwd)/Fish/config.fish ~/.config/fish/config.fish ;
 ln -s $(pwd)/Fish/functions/cd.fish ~/.config/fish/functions/cd.fish ;
+ln -s $(pwd)/Fish/functions/gitlog.fish ~/.config/fish/functions/gitlog.fish ;
 ln -s $(pwd)/Fish/aliases.fish ~/.config/fish/aliases.fish ;
+
 ln -s $(pwd)/I3/i3_config ~/.config/i3/config ;	
 ln -s $(pwd)/I3/i3exit ~/config/i3/i3exit ;	
 ln -s $(pwd)/I3/i3_status_config ~/.config/i3status/config ;
 ln -s $(pwd)/I3/polybar/launch.sh ~/.config/polybar/launch.sh ;
 ln -s $(pwd)/I3/polybar/config ~/.config/polybar/config ;
+ln -s $(pwd)/I3/polybar/scripts ~/.config/polybar/scripts ;
+
 ln -s $(pwd)/rofi ~/.config/rofi ;
+
 ln -s $(pwd)/tmux.conf ~/.tmux.conf ;	
 ln -s $(pwd)/gitmux.conf ~/.gitmux.conf ;	
+ln -s $(pwd)global_git_ignore ~/.global_git_ignore ;
+git config --global core.excludesfile ~/.global_git_ignore ;
+ln -s $(pwd)gitconfig ~/.gitconfig ;
+
 ln -s $(pwd)/taskrc ~/.taskrc ;	
-ln -s $(pwd)global_git_ignore ~/.global_git_ignore
+
 
