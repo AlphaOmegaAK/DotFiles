1cb1884f0a058b7d781752fc2f10e0bff15381bf
Prometheus <arturxkaminski@gmail.com>
2021-09-17 16:48:07 -0400

    Updated i3 config with new powermenu configuration, adjusted floating window behavior, fish: fzf bindings

---
+++ Fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/prometheus/.fzf/shell/key-bindings.fish
--- I3/i3lock/i3lock.sh
+++ I3/i3lock/i3lock.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
-set -eu
+#set -eu
 
-[[ -z "$(pgrep i3lock)" ]] || exit
-i3lock -n -u -t -i ${HOME}/.config/i3lock/lock.jpg
+#[[ -z "$(pgrep i3lock)" ]] || exit
+#i3lock -n -u -t -i ${HOME}/.config/i3lock/poly-triangles.png
+
+lock() {
+    i3lock -nf -i $HOME/.config/i3lock/poly-triangles.png
+}
+
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        lock && systemctl suspend
+        ;;
+    hibernate)
+        lock && systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
