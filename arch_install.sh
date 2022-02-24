c0d5c2d67b1745aa6b2ba9557e89402ff5f2990b
DevArtK <arturxkaminski@gmail.com>
2022-02-24 18:33:32 -0500

    basic install script + packages installed from git

---
+++ arch_install.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+yes | pacman -S \
+	alacritty		\
+	curl			\
+	dunst			\
+	emacs			\
+	fd				\
+	feh				\
+	firefox			\
+	fish			\
+	flameshot		\
+	font-manager	\
+	gawk			\
+	git				\
+	git-delta		\
+	grep			\
+	htop			\
+	lxappearance	\
+	luajit			\
+	mpv				\
+	neofetch		\
+	neovim			\
+	nmap			\
+	pulseaudio		\
+	playerctl		\
+	pulseaudio		\
+	ranger			\
+	ripgrep			\
+	sed				\
+	tree			\
+	tmux			\
+	ufw				\
+	vlc				\
+	wget			\
+
+
+
