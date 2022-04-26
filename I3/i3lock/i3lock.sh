8aeabe1838be663db8e9f08709a5f8439ccf56cd
DevArtK <arturxkaminski@gmail.com>
2022-04-26 14:51:27 -0400

    Refactor power menu, lock screen, code style

--- I3/i3lock/i3lock.sh
+++ I3/i3lock/i3lock.sh
@@ -1,35 +1,41 @@
 #!/usr/bin/env bash
-#set -eu
 
-#[[ -z "$(pgrep i3lock)" ]] || exit
-#i3lock -n -u -t -i ${HOME}/.config/i3lock/poly-triangles.png
+# lock() {
+#     i3lock -enfi -i $HOME/.config/i3lock/poly-triangles.png
+# }
 
-lock() {
-    i3lock -nf -i $HOME/.config/i3lock/poly-triangles.png
-}
+# case "$1" in
+#     lock)
+#         lock
+#         ;;
+#     logout)
+#         i3-msg exit
+#         ;;
+#     suspend)
+#         lock && systemctl suspend
+#         ;;
+#     hibernate)
+#         lock && systemctl hibernate
+#         ;;
+#     reboot)
+#         systemctl reboot
+#         ;;
+#     shutdown)
+#         systemctl poweroff
+#         ;;
+#     *)
+#         echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+#         exit 2
+# esac
 
-case "$1" in
-    lock)
-        lock
-        ;;
-    logout)
-        i3-msg exit
-        ;;
-    suspend)
-        lock && systemctl suspend
-        ;;
-    hibernate)
-        lock && systemctl hibernate
-        ;;
-    reboot)
-        systemctl reboot
-        ;;
-    shutdown)
-        systemctl poweroff
-        ;;
-    *)
-        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
-        exit 2
-esac
+# exit 0
 
-exit 0
+PICTURE=/tmp/i3lock.png
+SCREENSHOT="scrot $PICTURE"
+
+BLUR="5x4"
+$SCREENSHOT
+gm convert $PICTURE -blur $BLUR $PICTURE
+i3lock -efi $PICTURE
+
+sleep 60; pgrep i3lock && xset dpms force off
