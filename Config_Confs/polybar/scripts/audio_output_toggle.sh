b2fdb222f16136c1b93fce2c35689d0d93af8995
DevArtK <arturxkaminski@gmail.com>
2022-06-09 15:54:33 -0400

    added a audio output toggle shortcut between headphones and system speakers for polybar, conky seperated into two 'windows'

---
+++ Config_Confs/polybar/scripts/audio_output_toggle.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Written by DevArt
+
+# Usage :
+# Intended use for this short script is to either use an alias for it to switch
+# sinks from the command line or use it in a polybar module
+
+# How to setup :
+# 1. Find the desired sinks by running command : pactl list short sinks
+# 2. Fill the names of the sinks into variables
+# 3. Run script, create an alias, add to path (prob wanna rename), or use in another program to execute
+
+SPEAKERS="alsa_output.pci-0000_03_00.1.hdmi-stereo"
+HEADPHONES="alsa_output.usb-SteelSeries_Arctis_7P_-00.analog-stereo"
+
+RUNNING="RUNNING"
+
+SPEAKERS_A="$(pactl list short sinks | grep $SPEAKERS | cut -f 5)"
+HEADPHONES_B="$(pactl list short sinks | grep $HEADPHONES | cut -f 5)"
+
+
+if [ "$SPEAKERS_A" == "$RUNNING" ]; then
+	pactl set-default-sink "$HEADPHONES"
+	notify-send -u critical "Switched to Arctis Headphones"
+	exit 0
+else
+	pactl set-default-sink "$SPEAKERS"
+	notify-send -u critical "Switched to System Speakers"
+	exit 0
+fi
+
+switch_() {
+
+}
