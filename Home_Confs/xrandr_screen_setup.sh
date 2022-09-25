d0015f4004e709007266738cfee7f8de99053c9d
DevArtK <arturxkaminski@gmail.com>
2022-09-25 05:03:35 -0400

    Update to completions for zsh, i3 and xrander

--- Home_Confs/xrandr_screen_setup.sh
+++ Home_Confs/xrandr_screen_setup.sh
@@ -9,4 +9,7 @@
 # +X		       +Y
 # +Horizonal Offet +Vertical Offset
 # xrandr --output DP-1 --auto --primary -pos 0x0 --rotate normal --output HDMI-0 +0+1400 --rotate normal
-xrandr --output DisplayPort-3 --mode 3440x1440 --rate 100.00 --primary --pos 0x0 --rotate normal --output HDMI-A-1 --mode 1920x1080 --rate 120.00 --pos 750x-1425 --rotate normal
+xrandr --output DisplayPort-2 --mode 3440x1440
+
+
+# xrandr --output DisplayPort-2 --mode 3440x1440 --rate 100.00 --primary --pos 0x0 --rotate normal --output HDMI-A-1 --mode 1920x1080 --rate 120.00 --pos 750x-1425 --rotate normal
